20b3f4942ff7ecfec1fe7d649290fe2ba34e6692
Ale Aguirre <aguirrealessandro8@gmail.com>
2023-02-06 17:43:51 -0600

    tarea1

---
+++ Clase2 Ciclos y condiciones/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase2_Ciclos_y_condiciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String nombre = "Juan";
+            int? num1 = null;
+            var persona = new { nombre = "felipe", edad = 20 };
+            float valor1, valor2;
+            float salario;
+            var variable = true;
+
+            dynamic dinamico = 25;
+            dinamico = "cadena de caracteres";
+            dinamico = false;
+            Console.WriteLine(num1);
+
+
+
+            valor1 = 15f;
+            valor2 = 20f;
+            salario = 325.56f;
+
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"la suma de: {valor1} + {valor2} = {valor1 + valor2}");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"la multiplicacion de: {valor1} * {valor2} = {valor1 * valor2}");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"la multiplicacion de: {valor1} - {valor2} = {valor1 - valor2}");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"la multiplicacion de: {valor1} / {valor2} = {valor1 / valor2}");
+
+
+            float total = salario * salario * 0.13f;
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"elsalario de {persona.nombre} es de {salario} y el IVA es de {salario * 0.13} y el; total va a ser igual a: {total}");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Nombre: " + persona.nombre + " edad: " + persona.edad);
+            Console.WriteLine($"Nombre: {persona.nombre}  edad: {persona.edad}");
+            Console.WriteLine("nombre: {0} edad: {1}", persona.nombre, persona.edad);
+            Console.ReadLine();
+
+            //Arreglos y matrices
+
+
+            string[] musicos = new string[] {"ringo","george","paul","jhon"};
+
+            float[] salarios = new float[4];
+            salarios[0]= 500;
+            salarios[1]= 300;
+            salarios[2]= 100;
+            salarios[3]= 250;
+
+            float promedio = 0;
+
+
+            for (int i = 0; i <= 4; i++)
+            {
+                Console.WriteLine($"Musico:{musicos[i]} salario: {salarios[i]}");
+            }
+            Console.WriteLine($"el promedio de salarios es de: {0}",(promedio/=musicos.Length));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ClaseMatrices/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseMatrices
+{
+    internal class Program
+    {
+        public static float num1, num2;  
+        public static void datos()
+        
+        {
+
+            Console.WriteLine("digite  num1");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("digite num2");
+            num2 = int.Parse(Console.ReadLine());
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[3, 3];
+            int[,] matrix2 = new[,] { { 0, 1, 2 }, { 2, 3, 6 } };
+            float sumatoria = 0;
+            Console.WriteLine("**********  MATRIX  **************");
+            for (int f = 0; f < 3; f++) //filas
+            {
+                for (int c = 0; c < 3; c++) //coluimnas
+                {
+                    Console.WriteLine(matrix[f, c]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("********** MATRIX 2 **************");
+            for (int f = 0; f < 2; f++) //filas
+            {
+                for (int c = 0; c < 3; c++) //coluimnas
+                {
+                    Console.WriteLine(matrix2[f, c]);
+                    sumatoria += matrix2[f, c];
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Sumatoria  = {0}", sumatoria);
+            Console.WriteLine("promedio {0}", sumatoria /= matrix2.Length);
+
+
+
+
+            int salir = 0;
+            do
+            {
+                Console.WriteLine("1- sumar 1");
+                Console.WriteLine("2- multiplicar 2");
+                Console.WriteLine("3- restar 3"); //siclo tipo menu muestra el texto hasta que terminemos la condicion 
+                Console.WriteLine("4- dividir 4");
+                Console.WriteLine("digite una opcion: ");
+                salir = int.Parse(Console.ReadLine());
+
+
+
+
+
+                //ESTE ES EL EJERCICO CREO,  NO ENTENDI  BIEN SI ERA ESTE?
+
+
+
+                switch (salir)
+                {
+                    case 1:
+                        Console.WriteLine("digite  num1");
+                        num1 = int.Parse(Console.ReadLine());
+                        Console.WriteLine("digite num2");
+                        num2= int.Parse(Console.ReadLine());
+                        Console.WriteLine("total suma {0}", num1+num2);
+                        break;
+                    case 2:
+                        datos();
+                        Console.WriteLine("total multiplicacion {0}", num1 * num2);                      
+                        break;
+                    case 3:
+                        datos();
+                        Console.WriteLine("total resta {0}", num1 - num2);
+                        break;
+                    case 4:
+                        datos();
+                        Console.WriteLine("total division {0}", num1 / num2);
+                        break;
+                    default:
+                        Console.WriteLine("salir :v ");
+                        break;
+                }
+            } while (salir != 4);
+
+            
+ 
+
+            Console.WriteLine("quires hacerlo?");
+            String opcion =(Console.ReadLine());
+            while (opcion !="n")
+            {
+                Console.WriteLine("digite monto para el  prestamo: ");
+                float monto = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("digite el salario");
+                float salario = float.Parse(Console.ReadLine());
+
+                if ((monto >= 100000) && (salario < 100000))
+                {
+                    Console.WriteLine("no se le p[uede pagar");
+                }
+                else if ((monto >= 100000) && (salario >= 100000))
+                {
+                    Console.WriteLine("se le presta a 6  meses");
+                }
+                else if ((monto < 100000) && (salario >= 100000))
+                {
+                    Console.WriteLine("se le presta a 24  meses");
+                }
+                Console.WriteLine("desea continuar?");
+                opcion = (Console.ReadLine());
+            }          
+            Console.ReadLine();
+        }
+    }
+}
